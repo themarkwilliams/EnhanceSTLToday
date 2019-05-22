829573859c0bc0b516f589739fb749d0f6f17766
themarkwilliams <themarkwilliams@users.noreply.github.com>
2019-05-22 07:50:11 -0500

    1.5 - Fixed Overflow bug by removing last change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Enhance STLToday
-// @version 1.4
+// @version 1.5
 // @description "A user script to automatically bypass the paywall by marking the content as free."
 // @match http*://*.stltoday.com/*
 // @run-at document-start
@@ -12,6 +12,7 @@
 1.2 - Hide most elements required to show content
 1.3 - Attempt to remove the need to click to view the article. Also hide additional modal dialogs about Ad-Blocker - 5/9/19
 1.4 - Some articles have an overflow:hidden element that prevents scrolling. This attempts to fix that - 5/13/19
+1.5 - Overflow function was causing rest of script to fail. commented out - 5/22/19
 */
 
 GM_addStyle('.redacted-overlay { display:none !important; }'); // 4/11/18
@@ -19,22 +20,25 @@ GM_addStyle('.subscription-required { display:none !important; }'); // 4/11/18
 GM_addStyle('.meter message { display:none !important; }'); // 4/11/18
 GM_addStyle('.fc-ab-root {display:none !important;}'); //5/9/19
 GM_addStyle('.modal-backdrop {display:none !important;}'); //5/9/19
-(function() { //5/13/19 - Get rid of Overflow:hidden on body
-    'use strict';
-    // Credits: https://stackoverflow.com/questions/51330252/how-to-remove-the-css-rule-body-overflowhidden-automatically
-    document.body.style.overflow = "visible !important";
-})();
+
+//5/13/19 - Get rid of Overflow:hidden on body
+//document.body.style.overflow = "visible !important";
 
 // Reference: https://stackoverflow.com/questions/39884983/change-class-value-using-greasemonkey
 waitForKeyElements (".subscriber-only", swapClass);
 waitForKeyElements (".modal-open", removeModal); //5/9/19
 
 function swapClass (jNode) {
-    jNode.removeClass ("hide").addClass ("show");
+    console.log('Swap Class break...');
+    sleep(10000);
+    console.log('Ten seconds later, ...');
+
+    jNode.removeClass("hide");
+    jNode.addClass ("show");
     console.log("cleaned node : ", jNode);
 }
 
-function removeModal (jNode) {  //5/9/19
+function removeModal (jNode) { //5/9/19
     //Reference: https://www.w3schools.com/howto/howto_js_remove_class.asp
     console.log('Taking a break...');
     sleep(5000);
@@ -53,6 +57,6 @@ function removeModal (jNode) {  //5/9/19
     GM_addStyle('.lee-subscription-modal {display:none !important;}'); //5/9/19
 }
 
-function sleep(ms) {  //5/9/19
+function sleep(ms) { //5/9/19
   return new Promise(resolve => setTimeout(resolve, ms));
 }
