364eeea5d3d18d436b206abefe44d849cde0199a
themarkwilliams <themarkwilliams@users.noreply.github.com>
2020-01-17 07:57:20 -0600

    1.7 - Unscribbling some text

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Enhance STLToday
-// @version 1.6
+// @version 1.7
 // @description "A user script to automatically bypass the paywall by marking the content as free."
 // @match http*://*.stltoday.com/*
 // @run-at document-start
@@ -14,6 +14,7 @@
 1.4 - Some articles have an overflow:hidden element that prevents scrolling. This attempts to fix that - 5/13/19
 1.5 - Overflow function was causing rest of script to fail. commented out - 5/22/19
 1.6 - Take 2 of removing Overflow via function - 5/22/19
+1/7 - Unscribbled the redacted font family
 */
 
 GM_addStyle('.redacted-overlay { display:none !important; }'); // 4/11/18
@@ -21,6 +22,7 @@ GM_addStyle('.subscription-required { display:none !important; }'); // 4/11/18
 GM_addStyle('.meter message { display:none !important; }'); // 4/11/18
 GM_addStyle('.fc-ab-root {display:none !important;}'); //5/9/19
 GM_addStyle('.modal-backdrop {display:none !important;}'); //5/9/19
+GM_addStyle("p {font-family: Georgia, serif !important;}"); //1/17/20
 
 // Reference: https://stackoverflow.com/questions/39884983/change-class-value-using-greasemonkey
 waitForKeyElements (".subscriber-only", swapClass);
